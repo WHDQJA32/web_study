3441d9e5ef3c84ffc716f20898b97a45065cc19f
whdqja32 <whdqja32@naver.com>
2025-01-21 17:00:23 +0900

    관리자 페이지 사용자 목록 불러오기 추가

--- src/main/java/com/app/service/user/UserService.java
+++ src/main/java/com/app/service/user/UserService.java
@@ -1,8 +1,13 @@
 package com.app.service.user;
 
+import java.util.List;
+
+import com.app.dto.room.Room;
 import com.app.dto.user.User;
 
 public interface UserService {
+	
+	List<User> findUserList();
 	
 	int saveUser(User user);
 	
--- src/main/java/com/app/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/app/service/user/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.app.service.user.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.app.dao.user.UserDAO;
+import com.app.dto.room.Room;
 import com.app.dto.user.User;
 import com.app.service.user.UserService;
 
@@ -21,10 +24,9 @@ public class UserServiceImpl implements UserService {
 		return result;
 	}
 
-
 	@Override
 	public int saveCustomerUser(User user) {
-		//사용자 추가 전 검증 로직
+		// 사용자 추가 전 검증 로직
 		user.setUserType("CUS");
 		int result = userDAO.saveUser(user);
 		return result;
@@ -32,10 +34,19 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public int saveAdminUser(User user) {
-		//관리자 추가 전 검증 로직
+		// 관리자 추가 전 검증 로직
 		user.setUserType("ADM");
 		int result = userDAO.saveUser(user);
 		return result;
+	}
+
+	@Override
+	public List<User> findUserList() {
+		// TODO Auto-generated method stub
+
+		List<User> userList = userDAO.findUserList();
+		
+		return userList;
 	}
 
 }
