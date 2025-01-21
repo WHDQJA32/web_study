fdb408699be560b38d659fb1df013f438d6bf253
whdqja32 <whdqja32@naver.com>
2025-01-21 10:02:29 +0900

    호실 등록, 유저 등록 기능 및 구조 추가 진행 중

---
+++ src/main/java/com/app/controller/admin/AdminController.java
@@ -0,0 +1,56 @@
+package com.app.controller.admin;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.app.dto.room.Room;
+import com.app.service.room.RoomService;
+import com.app.service.user.UserService;
+
+@Controller
+public class AdminController {
+
+	@Autowired
+	RoomService roomService;
+
+	@Autowired
+	UserService userService;
+
+	// 객실 등록
+	@GetMapping("/admin/registerRoom")
+	public String registerRoom() {
+		return "admin/registerRoom";
+	}
+
+	@PostMapping("/admin/registerRoom")
+	public String registerRoomAction(Room room) {
+
+		// 값 넘어온거 확인
+		System.out.println(room.toString());
+		// 등록
+		int result = roomService.saveRoom(room);
+		System.out.println(result);
+
+		return "admin/registerRoom";
+	}
+
+	// 관리자 객실 목록 확인
+	@GetMapping("/admin/rooms")
+	public String rooms(Model model) {
+		
+		List<Room> roomList = roomService.findRoomList();
+		//service.findRoomList 호출 -> DAO findRoomList -> DB (Mybatis mapper) select
+		//                   <-List<Room> 			<-List<Room>
+		// Controller DB로 부터 조회 데이터 -> 화면 전달 -> 화면 출력(표시)
+		model.addAttribute("roomList", roomList);
+	
+		return "admin/rooms";
+	}
+
+	// 고객 관리/등록
+}
--- src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
+++ src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
@@ -2,6 +2,8 @@ package com.app.dao.room.impl;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.app.dao.room.RoomDAO;
@@ -12,13 +14,27 @@ import com.app.dto.room.Room;
 @Repository   //Bean 등록 Annotation
 public class RoomDAOImpl implements RoomDAO {
 
+	@Autowired			
+	SqlSessionTemplate sqlSessionTemplate;			
+
+	
 	@Override
 	public List<Room> findRoomList() {
 		
 		System.out.println("RoomDAO 호출 됨");
 		//db 연결 조회
+		List<Room> roomList = sqlSessionTemplate.selectList("room_mapper.findRoomList");
 		
-		return null;
+		return roomList;
+	}
+
+	@Override
+	public int saveRoom(Room room) {
+		// DB 에 전달받은 Room 객체에 들어있는 데이터를 잘~ 저장 ~
+		
+		int result = sqlSessionTemplate.insert("room_mapper.saveRoom", room);
+		
+		return result;
 	}
 
 }
