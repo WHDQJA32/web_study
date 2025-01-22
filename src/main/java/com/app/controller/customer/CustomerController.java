500625f1ca0927a2537d7d34fc4dfa59a38e8079
whdqja32 <whdqja32@naver.com>
2025-01-22 10:48:22 +0900

    회원 가입 추가

--- src/main/java/com/app/controller/customer/CustomerController.java
+++ src/main/java/com/app/controller/customer/CustomerController.java
@@ -2,7 +2,10 @@ package com.app.controller.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.app.dto.user.User;
 import com.app.service.user.UserService;
 
 @Controller
@@ -10,4 +13,41 @@ public class CustomerController {
 	
 	@Autowired
 	UserService userService;
+	
+	//회원가입
+	@GetMapping("/customer/signup")
+	public String signup() {
+		return "customer/signup";
+	}
+	
+	@PostMapping("/customer/signup")
+	public String signupAction(User user) {
+		user.setUserType("CUS");
+		int result = userService.saveUser(user);
+		
+		System.out.println("회원가입 처리 결과 : " + result);
+		
+		if(result > 0) {
+			return "redirect:/main";
+		} else {
+			return "customer/signup";			
+		}
+	}
+	
+	@GetMapping("/customer/login")
+	public String login() {
+		return "customer/login";
+	}
+	
+	@PostMapping("/customer/login")
+	public String loginAction(User user) {
+		
+		//user id pw 화면으로부터 전달
+		//name userType : null
+		
+		//user 로그인 할 수 있게 정보가 들어있는지 확인!
+		
+		return "customer/login";
+	}
 }
+	
